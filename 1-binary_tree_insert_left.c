71e9ea6e16e213df112df2c624d0bd4da7a2ca47
OUALIID <walidelhadim8@gmail.com>
2023-08-31 08:35:19 +0100

    تم اجراء بعض التعديلات على ملف 1-binary tree insert left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,13 +10,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	new = malloc(sizeof(binary_tree_t));
-	new->n = value;
+	new = binary_tree_node(parent, value);
 	if (new == NULL)
 		return (NULL);
-
-	new->parent = parent;
-	new->left = NULL;
 
 	if (parent == NULL)
 		return (NULL);
