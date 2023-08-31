fe81fdf2773ba004decc6922b2ee9f4740452a4b
OUALIID <walidelhadim8@gmail.com>
2023-08-31 15:15:54 +0100

    دالة تقيس عمق node في binary tree

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - measures the height of a depth tree
+ * @tree: Pointer to the node.
+ * Return: void
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->parent == NULL)
+		return (0);
+	size_t left_depth = binary_tree_depth(tree->parent);
+		return (left_depth + 1);
+}
