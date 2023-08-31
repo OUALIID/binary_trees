47b1b68ccd9e18054c6fb91c5d78d6a3f9cd63b0
OUALIID <walidelhadim8@gmail.com>
2023-08-31 08:12:00 +0100

    تمت اضافت كمنت للملف

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_insert_left - binary_tree_insert_left creation
+ * @parent: Pointer to the parent node
+ * @value: The value of the node
+ * Return: A pointer to the newly created node.
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
