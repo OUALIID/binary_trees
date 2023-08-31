d25057356f943fbfafff21c57e0669f426064cf4
OUALIID <walidelhadim8@gmail.com>
2023-08-31 18:16:48 +0100

    قمت بإجراء بعض التعديلات على الكود

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,11 +6,11 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t left_leaves = 0;
-
 	if (tree == NULL)
 		return (0);
 
-	left_leaves = binary_tree_leaves(tree->left);
-	return (left_leaves + 1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
